fa123c87d78aa4f8222fe95d027152043b383af7
HenriqueSenaDev <acopsenadeveloper>
2024-11-08 22:05:28 -0300

    front-office/feat: font assets configuration

---
+++ front-office/src/assets/fonts/index.ts
@@ -0,0 +1,17 @@
+import { Petrona, Plus_Jakarta_Sans, Lobster_Two } from 'next/font/google';
+
+export const petronaFontFamily = Petrona({
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+export const plusJakartaSansFontFamily = Plus_Jakarta_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+export const lobsterTwoFontFamily = Lobster_Two({
+  weight: '700',
+  display: 'swap',
+  subsets: ['latin'],
+});
